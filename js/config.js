d9570c407025dd1a96f108716179eebe8505218b
Leonardo Rangel <49524102+leonardo-rangel159@users.noreply.github.com>
2025-12-08 01:56:51 +0000

    atualização url

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 // Use esta URL após publicar como Web App
-const API_URL = 'https://script.google.com/macros/s/AKfycbwbnvbjQL8Z1khXKYH39_-TF6U7hIDxW7zRQpFDAafpVQ2VMW8cqh6WMVm4JXPZB-yfgA/exec';
+const API_URL = 'https://script.google.com/macros/s/AKfycbyvzeW9jorp2VKu8ps0TLYx1SqCTXxjjmrAxq5CeXloCLHBEBsXHu4lHlNH9RmtbnF1Sg/exec';
 
 // URL do seu Google Apps Script (substitua pela sua)
-const GOOGLE_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbynRsRthaNU9XhllGfmex2RIgFYw8XV0T0aT5Da0-Vg5IJ62ObaXsiXK4qwC2cjxa-ceg/exec';
+const GOOGLE_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbyvzeW9jorp2VKu8ps0TLYx1SqCTXxjjmrAxq5CeXloCLHBEBsXHu4lHlNH9RmtbnF1Sg/exec';
