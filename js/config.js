9a0b537bd624ab878f097dd46b1eb825ff2fd2aa
Leonardo Rangel <49524102+leonardo-rangel159@users.noreply.github.com>
2025-12-08 19:29:40 +0000

    cofig modou de pedido

--- js/config.js
+++ js/config.js
@@ -2,4 +2,4 @@
 const API_URL = 'https://script.google.com/macros/s/AKfycbyvzeW9jorp2VKu8ps0TLYx1SqCTXxjjmrAxq5CeXloCLHBEBsXHu4lHlNH9RmtbnF1Sg/exec';
 
 // URL do seu Google Apps Script (substitua pela sua)
-const GOOGLE_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbyvzeW9jorp2VKu8ps0TLYx1SqCTXxjjmrAxq5CeXloCLHBEBsXHu4lHlNH9RmtbnF1Sg/exec';
+const GOOGLE_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbzBBXxoQTrLZDD8H0nDHCe6f7qN_U0JX12x46_qCgskpzif4Yk1hV5OJaPu-JqokM9KrQ/exec';
