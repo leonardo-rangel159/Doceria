445ea8ccd946bf2fdb776575813c29305b9b80f5
Leonardo Rangel <49524102+leonardo-rangel159@users.noreply.github.com>
2025-12-08 00:11:44 +0000

    url mudou

--- js/config.js
+++ js/config.js
@@ -2,4 +2,4 @@
 const API_URL = 'https://script.google.com/macros/s/AKfycbwbnvbjQL8Z1khXKYH39_-TF6U7hIDxW7zRQpFDAafpVQ2VMW8cqh6WMVm4JXPZB-yfgA/exec';
 
 // URL do seu Google Apps Script (substitua pela sua)
-const GOOGLE_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbwbnvbjQL8Z1khXKYH39_-TF6U7hIDxW7zRQpFDAafpVQ2VMW8cqh6WMVm4JXPZB-yfgA/exec';
+const GOOGLE_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbynRsRthaNU9XhllGfmex2RIgFYw8XV0T0aT5Da0-Vg5IJ62ObaXsiXK4qwC2cjxa-ceg/exec';
